eea26ff727cc4677585793b0206474c126c035f7
NIKUNJ PATIL <2024.nikunj.patil@ves.ac.in>
2026-07-06 21:13:50 +0530

    loops done

---
+++ Loops/Loops10.java
@@ -0,0 +1,19 @@
+public class Loops10 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=4;i++){
+            // for(int j=1;j<=4;j++){
+            //     if(i+j>4){
+            //         System.out.print("*");
+            //     }
+            //     else{
+            //         System.out.print(" ");
+            //     }
+            // }
+            for(int t = ; t<=4;t++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
---
+++ Loops/Loops9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Loops9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=n ; i>=1; i--){
+         
+            for(int j=1; j<= n; j++){
+               if(n<i+j){
+                System.err.print("*");
+               }
+               else{
+                System.out.print(" ");
+               }
+            }
+            System.out.println("");
+        }
+    }
+}
